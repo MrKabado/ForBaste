f6c48626bfdf45f032b49f460981ef1efafc6f2a
MrKabado <jersonjaybonghanoy@gmail.com>
2025-05-08 00:09:02 +0800

    Initial commit

---
+++ code/script.js
@@ -0,0 +1,28 @@
+const btnYes = document.getElementById('btnYes');
+const btnNo = document.getElementById('btnNo');
+const questionContainer = document.getElementById('questionContainer');
+const popupMessage = document.getElementById('popupMessage');
+const img = document.getElementById('image');
+const body = document.getElementById('body');
+
+btnNo.addEventListener("mouseover", () => {
+  const containerRect = questionContainer.getBoundingClientRect();
+  const maxX = containerRect.width - btnNo.offsetWidth;
+  const maxY = containerRect.height - btnNo.offsetHeight;
+
+  const newX = Math.floor(Math.random() * maxX);
+  const newY = Math.floor(Math.random() * maxY);
+
+  btnNo.style.position = "absolute";
+  btnNo.style.left = `${newX}px`;
+  btnNo.style.top = `${newY}px`;
+});
+
+btnYes.addEventListener("click", () => {
+  popupMessage.style.display = "block";
+  img.style.display = "block";
+  btnNo.style.display = "none";
+  btnYes.style.display = "none";
+  body.style.backgroundImage = "url('/code/bg.jpg')";
+  questionContainer.style.marginTop = "70%";
+});
